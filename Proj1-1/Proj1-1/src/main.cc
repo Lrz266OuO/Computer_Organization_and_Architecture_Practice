fc4b26d499a9f0f04e81276b3f9450ce8649d5db
LiRunze <lirunze.me@gmail.com>
2022-09-10 03:20:14 -0700

    update makefile

--- Proj1-1/Proj1-1/src/main.cc
+++ Proj1-1/Proj1-1/src/main.cc
@@ -2,12 +2,24 @@
  * @Author: LiRunze lirunze.me@gmail.com
  * @Date: 2022-09-09 05:27:01
  * @LastEditors: LiRunze
- * @LastEditTime: 2022-09-09 05:59:36
+ * @LastEditTime: 2022-09-10 03:13:19
  * @Description:  
  */
 
-#include "world.h"
+// #include "world.h"
+#include "cache.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+
+Cache c;
 
 int main(int argc, char *argv[]) {
 	// hello_world(argc, argv);
+	if(argc < 6) {
+		// invalid command
+		printf();
+		return 0;
+	}
 }
