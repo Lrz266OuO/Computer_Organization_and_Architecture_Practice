6d6184693aa235e1253b8119d58bf971ae30b386
LiRunze <lirunze.me@gmail.com>
2022-09-12 01:10:29 -0700

    update cache.h

--- Proj1-2/Proj1-2/src/cache.cc
+++ Proj1-2/Proj1-2/src/cache.cc
@@ -2,6 +2,12 @@
  * @Author: LiRunze lirunze.me@gmail.com
  * @Date: 2022-09-12 00:05:42
  * @LastEditors: LiRunze
- * @LastEditTime: 2022-09-12 00:05:43
+ * @LastEditTime: 2022-09-12 01:09:28
  * @Description:  
  */
+
+#include "cache.h"
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
--- Proj1-2/Proj1-2/src/cache.h
+++ Proj1-2/Proj1-2/src/cache.h
@@ -2,7 +2,7 @@
  * @Author: LiRunze lirunze.me@gmail.com
  * @Date: 2022-09-12 00:05:34
  * @LastEditors: LiRunze
- * @LastEditTime: 2022-09-12 00:52:08
+ * @LastEditTime: 2022-09-12 01:07:58
  * @Description:  
  */
 
@@ -15,6 +15,8 @@ class Cache {
         unsigned int BLOCKSIZE;             // block capacity of cache
         unsigned int SIZE;                  // total capacity of cache
         unsigned int ASSOC;                 // connectivity of cache
+        unsigned int SET;
+        unsigned int TAG;
 
         unsigned int NUM_OF_READ;           // number of read
         unsigned int NUM_OF_READ_MISS;      // number of read_miss
@@ -22,7 +24,6 @@ class Cache {
         unsigned int NUM_OF_WRITE_MISS;     // number of write_miss
         unsigned int NUM_OF_WRITE_BACK;     // number of write_back
         unsigned int NUM_OF_SWAP;           // number of swap
-      
         unsigned int TOTAL_MEMORY_TRAFFIC;  // total memory traffic between cache and main memory
 
         double MISS_RATE;                   // miss rate        = (readmisses+writemisses)/(reads+writes)
@@ -35,7 +36,24 @@ class Cache {
 
         Cache *nextLevel;                   // pointer to the next level of cache
 
+        unsigned int INDEX;
+        unsigned int TAG_LOC;
+        unsigned int TAG_ADD;
 
+        unsigned int *TAGS;
+        unsigned int *DIRTY;
+        unsigned int *VALID;
+        // unsigned int *LRU_C;
+
+        // int block;
+        // int index;
+
+        void init(unsigned int block, unsigned int size, unsigned int assoc, unsigned int set, unsigned int tag);
+        void transformAddress(unsigned int add);
+        void hit(int index);
+        void miss();
+
+        friend class CACHE;
 
 };
 
@@ -46,6 +64,14 @@ class CACHE {
         Cache Victim_Cache;                 // Victim cache
         char *TRACE_FILE;                   // filename of address stream
 
+    public:
+        void init(unsigned int block, unsigned int size1, unsigned int assoc1, unsigned int size2, unsigned int assoc2, unsigned int size_victim, char *tracefile);
+        void input();
+        void output();
+        
+        void readFromAddress(Cache &c, unsigned int add, unsigned int vc);
+        void writeToAddress(Cache &c, unsigned int add, unsigned int vc);
+        void readFromVictim(Cache &c, unsigned int add, char rw);
 
 };
 
